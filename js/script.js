10a09968a153a4b7a2dd1285833498a573ed99cc
thanhtan2810 <53827504+thanhtan2810@users.noreply.github.com>
2019-10-15 07:46:40 +0700

    doan1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav(){
+     document.getElementById("mysidebar").style.width="400px";
+     document.getElementById("items").style.marginRight="400px";
+}
+function closeNav() {
+    document.getElementById("mysidebar").style.width = "0";
+    document.getElementById("items").style.marginLeft= "0";
+     document.getElementById("nav").style.left="0";
+  }
